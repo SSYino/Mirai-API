cb26dbb4842816b1cf1a4ef6644e90dd067b3a4c
YuzuZensai <contact@kirameki.pink>
2021-08-21 08:12:36 -0700

    Added API v1 Routes

---
+++ src/routes/API/v1.ts
@@ -0,0 +1,42 @@
+import * as express from 'express';
+import Environment from '../../providers/Environment'
+
+// TODO: Implement GoogleAPIs and others routes
+//import Login from '../../controllers/API/v1/Auth/Login';
+
+import RateLimit from '../../middlewares/RateLimit';
+
+// TODO: Implement GoogleAPIs and others routes
+/*
+import Sessions from '../../middlewares/Sessions';
+import Me from '../../controllers/API/v1/Users/Me';
+import Courses from '../../controllers/API/v1/Classrooms/Courses';
+import CourseWorks from '../../controllers/API/v1/Classrooms/CourseWorks';
+import AdminUsers from '../../controllers/API/v1/Admin/Users';
+import Users from '../../middlewares/Users';
+*/
+
+const router = express.Router();
+
+let isDevEnv = (req: any, res: any, next: any) => {
+    if(Environment.get().NODE_ENV == 'development') {
+        return next();
+    }
+    else {
+        return res.sendStatus(403);
+    }
+};
+
+// TODO: Implement GoogleAPIs and others routes
+/*
+router.get('/auth/login', RateLimit.defaultAPI, Login.perform);
+
+router.get('/users/me', RateLimit.defaultAPI, Sessions.isAuthenticated, Me.perform);
+
+router.get('/admin/users', RateLimit.defaultAPI, Sessions.isAuthenticated, Users.isAdmin, AdminUsers.perform);
+
+router.get('/classrooms/courses', RateLimit.defaultAPI, Sessions.isAuthenticated, Courses.perform);
+router.get('/classrooms/courseworks', RateLimit.defaultAPI, Sessions.isAuthenticated, CourseWorks.perform);
+*/
+
+export default router;
