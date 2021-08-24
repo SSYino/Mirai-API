26832f45564115609323bb94b0c58da34bed22e8
YuzuZensai <contact@kirameki.pink>
2021-08-24 09:57:12 -0700

    Added more API scopes

--- src/providers/GoogleAPI.ts
+++ src/providers/GoogleAPI.ts
@@ -10,7 +10,10 @@ class GoogleAPI {
     public scopes: any = [
         'https://www.googleapis.com/auth/userinfo.profile',
         'https://www.googleapis.com/auth/classroom.courses',
-        'https://www.googleapis.com/auth/classroom.coursework.me'
+        'https://www.googleapis.com/auth/classroom.coursework.me',
+        'https://www.googleapis.com/auth/gmail.readonly',
+        'https://www.googleapis.com/auth/calendar.readonly'
+
     ];
 
     public google: any;
