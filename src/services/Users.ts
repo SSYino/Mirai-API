919fe4a646e57ddac4c7497431c0b68a1ae5a5a4
YuzuZensai <contact@kirameki.pink>
2021-08-25 09:47:27 -0700

    Updated to new error handling

--- src/services/Users.ts
+++ src/services/Users.ts
@@ -1,12 +1,13 @@
-
-
 import Environment from '../providers/Environment';
 import GoogleAPI from "../providers/GoogleAPI";
 import Prisma from "../providers/Prisma";
 
+import { ServiceError, DatabaseError } from "../exception/Errors"
+
 import Validator from "validator";
 import Sessions from './Sessions';
 import Logger from '../libs/Logger';
+import HTTP_STATUS from '../libs/HTTPStatus';
 
 const WHITELIST = [
 
@@ -35,43 +36,30 @@ class Users {
 
     }
 
-    public static async createUser(id: string, given_name: string, family_name: string, picture_url: string) {
+    public static async createUser(id: string, email: string, given_name: string, family_name: string, picture_url: string) {
 
-        if(!this.isGoogleIdValid(id).result)
-            return {
-                success: false,
-                error: this.isGoogleIdValid(id).error
-            }
+        if(!this.isGoogleIdValid(id))
+            throw new ServiceError(HTTP_STATUS.BAD_REQUEST, 'Invalid GoogleID');
 
         let doesExist = await Users.exists(id);
 
-        if(typeof doesExist.error !== "undefined")
-            return {
-                success: false,
-                error: doesExist.error
-            }
-    
-        if (doesExist.result)
-            return {
-                success: false,
-                error: "Google ID already exists"
-            }
+        if (doesExist)
+            throw new ServiceError(HTTP_STATUS.CONFLICT, 'Google ID already exists');
 
         const result = await Prisma.client.users.create({
             data: {
                 id: id,
                 given_name: given_name,
                 family_name: family_name,
+                email: email,
                 picture_url: picture_url,
             },
         });
 
         if (result == null)
-            throw new Error("User creation failed, result is null");
+            throw new DatabaseError("User creation failed, result is null");
 
-        return {
-            success: true
-        };
+        return true;
     }
 
     public static async authenticate(code: string) {
@@ -80,105 +68,63 @@ class Users {
         const oAuth2Client = new GoogleAPI.google.auth.OAuth2(
             credentials.web.client_id, credentials.web.client_secret, credentials.web.redirect_uris[0]);
 
-        let profile;
+        let token, profile;
 
         try {
-
-            let token = await oAuth2Client.getToken(code);
-            await oAuth2Client.setCredentials(token.tokens);
-        
-            const oauth2 = GoogleAPI.google.oauth2({
-                auth: oAuth2Client,
-                version: 'v2'
-            });
-
-            profile = await oauth2.userinfo.get();
-
-            if(!this.isWhitelisted(profile.data.id)) {
-                return {
-                    success: false,
-                    error: "You're not whitelisted. If you wish to access the application, please contact the developer and give them your user id: " + profile.data.id
-                }
-            }
-
-            let doesExist = await Users.exists(profile.data.id);
-
-            if(typeof doesExist.error !== "undefined")
-                return {
-                    success: false,
-                    error: doesExist.error
-                }
-    
-            if (!doesExist.result) {
-                let result = await Users.createUser(profile.data.id, profile.data.given_name, profile.data.family_name, profile.data.picture);
-                
-                if(!result.success)
-                    throw result.error;
-            }
-
-            let issueToken = await Sessions.issueToken(profile.data.id, token.tokens.access_token, token.tokens.refresh_token, token.tokens.expiry_date);
-            
-            if(!issueToken.success)
-                return {
-                    success: false,
-                    error: issueToken.error
-                }
-
-            return {
-                success: true,
-                token: issueToken.token
-            }
-          
-            /*profile = await GoogleAPI.google.people('v1').people.get({
-                resourceName: 'people/me',
-                personFields: 'emailAddresses,names',
-                auth: oAuth2Client,
-            });*/
-
-
-        } catch(err: any) {
-
-            if(err || !err.message) {
-                
-                Logger.log('warn', 'Unable to authenticate');
-                Logger.log('warn', err);
-
-                return({
-                    success: false,
-                    error: "Unable to authenticate"
-                });
-            }
-            else if(err.message.includes("invalid_grant")) {
-                
-                return({
-                    success: false,
-                    error: "Invalid login details, please try again"
-                });
-            }
-            else {
-                
-                Logger.log('warn', 'Unable to authenticate');
-                Logger.log('warn', err);
-
-                return({
-                    success: false,
-                    error: "Unable to authenticate"
-                });
-            }
-
+            token = await oAuth2Client.getToken(code);
+        } catch (err: any) {
+            if(err?.response?.data?.error === "invalid_grant")
+                throw new ServiceError(HTTP_STATUS.UNAUTHORIZED, "Invalid Google OAuth2 grant");
+            else
+                throw err;
         }
 
+        await oAuth2Client.setCredentials(token.tokens);
+
+        const oauth2 = GoogleAPI.google.oauth2({
+            auth: oAuth2Client,
+            version: 'v2'
+        });
+
+        profile = await oauth2.userinfo.get();
+
+        if(!this.isWhitelisted(profile.data.id))
+            throw new ServiceError(HTTP_STATUS.FORBIDDEN, `GoogleID not whitelisted, If you wish to access the application, please contact the developer and give them your user id (${profile.data.id})`);        
+        
+        let doesExist = await Users.exists(profile.data.id);        
+        let gmail = await GoogleAPI.google.gmail('v1').users.getProfile({
+            userId: profile.data.id,
+            auth: oAuth2Client,
+        });
+
+
+        if (!doesExist) {
+            await Users.createUser(profile.data.id, gmail.data.emailAddress, profile.data.given_name, profile.data.family_name, profile.data.picture);
+        } else {
+            // TODO: Check if user data was changed and update it on the database.
+        }
+
+        const issuedToken = await Sessions.issueToken(profile.data.id, token.tokens.access_token, token.tokens.refresh_token, token.tokens.expiry_date);
+            
+        return {
+            success: true,
+            token: issuedToken,
+            profile: {
+                family_name: profile.data.family_name,
+                given_name: profile.data.given_name,
+                id: profile.data.id,
+                locale: profile.data.locale,
+                name: profile.data.name,
+                picture: profile.data.picture,
+                emailAddress: gmail.data.emailAddress
+            }
+        }
     }
 
     public static async getUser(id: string) {
+        if(!this.isGoogleIdValid(id))
+            throw new ServiceError(HTTP_STATUS.BAD_REQUEST, 'Invalid GoogleID');
 
-
-        if(!this.isGoogleIdValid(id).result)
-            return {
-                success: false,
-                error: this.isGoogleIdValid(id).error
-            }
-            
         const result = await Prisma.client.users.findUnique({
             where: {
                 id: id
@@ -186,42 +132,20 @@ class Users {
         });
 
         if (result == null)
-            return {
-                success: false,
-                error: "User does not exist"
-            };
+            throw new ServiceError(HTTP_STATUS.NOT_FOUND, 'User does not exist');
 
-        return {
-            success: true,
-            data: result
-        };
-
+        return result;
     }
 
     public static async getAllUser() {
-
         const result = await Prisma.client.users.findMany();
-
-        if (result == null)
-            return {
-                success: false,
-                error: "No users exist"
-            };
-
-        return {
-            success: true,
-            data: result
-        };
-
+        return result;
     }
 
     public static async exists(id: string) {
 
-        if(!this.isGoogleIdValid(id).result)
-            return {
-                result: false,
-                error: this.isGoogleIdValid(id).error
-            }
+        if(!this.isGoogleIdValid(id))
+            throw new ServiceError(HTTP_STATUS.BAD_REQUEST, 'Invalid GoogleID');
 
         const result = await Prisma.client.users.findUnique({
             where: {
@@ -230,28 +154,17 @@ class Users {
         });
     
         if (result == null)
-            return {
-                result: false
-            };
+            return false;
         
-        return {
-            result: true
-        };
+        return true;
 
     }
 
     public static isGoogleIdValid(id: string) {
-
         if (!Validator.isNumeric(id))
-            return {
-                result: false,
-                error: "Invalid Google ID"
-            };
+            return false;
 
-        return {
-            result: true
-        };
-
+        return true;
     }
 
     public static isWhitelisted(id: string) {
@@ -261,25 +174,12 @@ class Users {
 
     public static async isAdmin(id: string) {
 
-        if (!Validator.isNumeric(id))
-            return {
-                result: false,
-                error: "Invalid Google ID"
-            };
+        if(!this.isGoogleIdValid(id))
+            throw new ServiceError(HTTP_STATUS.BAD_REQUEST, 'Invalid GoogleID');
 
         let user = await this.getUser(id);
-        if(!user.success) {
-            return {
-                result: false,
-                error: user.error
-            };
-        }
-        
-        else {
-            return {
-                result: user.data?.isAdmin
-            };
-        }
+
+        return user.isAdmin;
     }
     
 }
