c96a1117119555b820b6c2d35b17f17ad82ef192
YuzuZensai <contact@kirameki.pink>
2021-08-25 09:45:45 -0700

    Wrapped around Promise instead

--- src/controllers/API/v1/Auth/Login.ts
+++ src/controllers/API/v1/Auth/Login.ts
@@ -4,20 +4,23 @@ import RequestHelper from "../../../../libs/RequestHelper";
 import Users from "../../../../services/Users";
 
 class Login {
-    public static async perform(req: any, res: any) {
+    public static perform(req: any, res: any, next: any) {
+        Promise.resolve().then(async () => {
+            if (typeof req.query != "undefined" && typeof req.query.code != "undefined") {
+                const code = req.query.code;
+                
+                let result = await Users.authenticate(code)
+                res.json(result);
+            }
+            
+            else {
+                return res.status(200).json({
+                    success: true,
+                    url: Users.generateAuthenticateURL()
+                });
+            }
 
-        if (typeof req.query != "undefined" && typeof req.query.code != "undefined") {
-            const code = req.query.code;
-            res.json(await Users.authenticate(code));
-        }
-
-        else {
-            return res.status(200).json({
-                success: true,
-                url: Users.generateAuthenticateURL()
-            });
-        }
-
+        }).catch(next)
     }
 }
 
