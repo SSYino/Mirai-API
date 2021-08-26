efe3b93dd5ec8739989e90e1c34fbe4c0b7b122c
YuzuZensai <contact@kirameki.pink>
2021-08-26 09:10:13 -0700

    Disabled broken APIs

--- src/routes/API/v1.ts
+++ src/routes/API/v1.ts
@@ -7,9 +7,9 @@ import RateLimit from '../../middlewares/RateLimit';
 
 import Sessions from '../../middlewares/Sessions';
 import Me from '../../controllers/API/v1/Users/Me';
-import Courses from '../../controllers/API/v1/Classrooms/Courses';
-import CourseWorks from '../../controllers/API/v1/Classrooms/CourseWorks';
-import AdminUsers from '../../controllers/API/v1/Admin/Users';
+//import Courses from '../../controllers/API/v1/Classrooms/Courses';
+//import CourseWorks from '../../controllers/API/v1/Classrooms/CourseWorks';
+//import AdminUsers from '../../controllers/API/v1/Admin/Users';
 import Users from '../../middlewares/Users';
 
 
@@ -29,10 +29,10 @@ router.get('/auth/login', RateLimit.defaultAPI, Login.perform);
 
 router.get('/users/me', RateLimit.defaultAPI, Sessions.isAuthenticated, Me.perform);
 
-router.get('/admin/users', RateLimit.defaultAPI, Sessions.isAuthenticated, Users.isAdmin, AdminUsers.perform);
+//router.get('/admin/users', RateLimit.defaultAPI, Sessions.isAuthenticated, Users.isAdmin, AdminUsers.perform);
 
-router.get('/classrooms/courses', RateLimit.defaultAPI, Sessions.isAuthenticated, Courses.perform);
-router.get('/classrooms/courseworks', RateLimit.defaultAPI, Sessions.isAuthenticated, CourseWorks.perform);
+//router.get('/classrooms/courses', RateLimit.defaultAPI, Sessions.isAuthenticated, Courses.perform);
+//router.get('/classrooms/courseworks', RateLimit.defaultAPI, Sessions.isAuthenticated, CourseWorks.perform);
 
 
 export default router;
