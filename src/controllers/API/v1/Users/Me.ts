17dd7ca0948c39ac035bf83a80a8249b7e54aa13
YuzuZensai <contact@kirameki.pink>
2021-08-26 09:09:40 -0700

    Fixed Me API

--- src/controllers/API/v1/Users/Me.ts
+++ src/controllers/API/v1/Users/Me.ts
@@ -6,47 +6,24 @@ import Sessions from "../../../../services/Sessions";
 import Users from "../../../../services/Users";
 
 class Me {
-    public static async perform(req: any, res: any) {
+    public static async perform(req: any, res: any, next: any) {
+        Promise.resolve().then(async () => {
+            let reqToken = Sessions.extractTokenHeader(req);
+            let sessionOwner = await Sessions.getTokenOwner(reqToken);
 
-        let reqToken = Sessions.extractTokenHeader(req);
+            let User = await Users.getUser(sessionOwner);
 
-        if(!reqToken.success)
-            return res.status(400).json({
-                success: false,
-                error: Sessions.extractTokenHeader(req).error
-            });     
-
-        let sessionOwner = await Sessions.getTokenOwner(reqToken.token);
-
-        if(!sessionOwner.success)
-            return res.status(400).json({
-                success: false,
-                error: sessionOwner.error
+            return res.status(200).json({
+                profile: {
+                    id: User.id,
+                    email: User.email,
+                    picture_url: User.picture_url,
+                    given_name: User.given_name,
+                    family_name: User.family_name,
+                    createdAt: User.createdAt
+                }
             });
-
-        if(typeof sessionOwner.id == "undefined")
-            throw new Error("Unable to lookup users, session owner object is null");
-
-        let User = await Users.getUser(sessionOwner.id);
-
-        if(!User.success)
-            return res.status(400).json({
-                success: false,
-                error: User.error
-            });
-
-        if(typeof User.data == "undefined")
-            throw new Error("Unable to lookup users, user object is null");
-            
-        return res.status(200).json({
-            success: true,
-            data: {
-                id: User.data.id,
-                given_name: User.data.given_name,
-                family_name: User.data.family_name,
-                picture_url: User.data.picture_url,
-            }
-        });
+        }).catch(next);
     }
 }
 
