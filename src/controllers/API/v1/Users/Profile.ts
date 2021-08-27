4f74f03d3fa0a20a4a7d62c1d2bc46d038591bb3
YuzuZensai <contact@kirameki.pink>
2021-08-27 11:11:36 -0700

    Renamed to Profile

--- src/controllers/API/v1/Users/Profile.ts
+++ src/controllers/API/v1/Users/Profile.ts
@@ -4,19 +4,24 @@ import Logger from "../../../../libs/Logger";
 
 import Sessions from "../../../../services/Sessions";
 import Users from "../../../../services/Users";
+import { ServiceError } from "../../../../exception/Errors";
+import HTTP_STATUS from "../../../../libs/HTTPStatus";
 
-class Me {
+class Profile {
     public static async perform(req: any, res: any, next: any) {
         Promise.resolve().then(async () => {
-            let reqToken = Sessions.extractTokenHeader(req);
-            let sessionOwner = await Sessions.getTokenOwner(reqToken);
+            //let reqToken = Sessions.extractTokenHeader(req);
+            ///let sessionOwner = await Sessions.getTokenOwner(reqToken);
 
-            let User = await Users.getUser(sessionOwner);
+            if(!Users.isGoogleIdValid(req.params.id))
+                throw new ServiceError(HTTP_STATUS.BAD_REQUEST, "Invalid GoogleID")
+
+            let User = await Users.getUser(req.params.id);
 
             return res.status(200).json({
                 profile: {
                     id: User.id,
-                    email: User.email,
+                    //email: User.email,
                     picture_url: User.picture_url,
                     given_name: User.given_name,
                     family_name: User.family_name,
@@ -27,4 +32,4 @@ class Me {
     }
 }
 
-export default Me;
+export default Profile;
