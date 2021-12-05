4a4cc9c0c9f198352c2a86ab207eb846077df44a
YuzuZensai <contact@kirameki.pink>
2021-12-05 11:50:49 -0800

    Update Users.ts

--- src/controllers/API/v1/Admin/Users.ts
+++ src/controllers/API/v1/Admin/Users.ts
@@ -9,29 +9,46 @@ import HTTP_STATUS from "../../../../libs/HTTPStatus";
 class Users {
     public static async perform(req: any, res: any, next: any) {
         Promise.resolve().then(async () => {
-        //let reqToken = Sessions.extractTokenHeader(req);
-        //let sessionOwner = await Sessions.getTokenOwner(reqToken.token);
-        
-        let AllUsers = await UserService.getAllUser();
 
-        let userData = [];
-        for(let user of AllUsers) {
-            userData.push({
-                profile: {
-                    family_name: user.family_name,
-                    given_name: user.given_name,
-                    id: user.id,
-                    picture_url: user.picture_url,
-                    email: user.email
-                },
-                isAdmin: user.isAdmin,
-                isDeveloper: user.isDeveloper,
-                isTeacher: user.isTeacher,
-                isStudent: user.isStudent
-            });
-        }
+            let AllUsers: any;
+
+            if(!req.query)
+                AllUsers = await UserService.getAllUser({});
+            else
+                AllUsers = await UserService.getAllUser(req.query);
+     
+            let userData = [];
+            for(let user of AllUsers) {
+                userData.push({
+                    profile: {
+                        family_name: user.family_name,
+                        given_name: user.given_name,
+                        id: user.id,
+                        picture_url: user.picture_url,
+                        email: user.email
+                    },
+                    isAdmin: user.isAdmin,
+                    isDeveloper: user.isDeveloper,
+                    isTeacher: user.isTeacher,
+                    isStudent: user.isStudent,
+                    isSuspended: user.isSuspended
+                });
+            }
+
+            if(req.query) {
+                if(!req.query.role && !req.query.search && !req.query.status)
+                    return res.status(HTTP_STATUS.OK).json({
+                        total: (await UserService.getAllUser({})).length,
+                        users: userData
+                    });
+                else
+                    return res.status(HTTP_STATUS.OK).json({
+                        total: AllUsers.length,
+                        users: userData
+                    });
+            }   
+            return res.status(HTTP_STATUS.OK).json(userData);
             
-        return res.status(HTTP_STATUS.OK).json(userData);
         }).catch(next);
     }
 }
